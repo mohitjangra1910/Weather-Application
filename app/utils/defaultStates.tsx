42e58aa0ea39a9973cca773fbfb69d1e64f99d37
Mohit <102729870+MohitJangra7@users.noreply.github.com>
2024-05-28 00:43:55 +0530

    Add files via upload

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Chandigarh",
+    country: "IN",
+    state: "Chandigarh",
+    lat: 30.7333,
+    lon: 76.7794,
+  },
+  {
+    name: "New Delhi",
+    country: "IN",
+    state: "Delhi",
+    lat: 28.6139,
+    lon: 77.209,
+  },
+  {
+    name: "Mumbai",
+    country: "IN",
+    state: "Maharashtra",
+    lat: 19.076,
+    lon: 72.8777,
+  },
+  {
+    name: "Kolkata",
+    country: "IN",
+    state: "West Bengal",
+    lat: 22.5726,
+    lon: 88.3639,
+  },
+  {
+    name: "Bengaluru",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+];
+
+export default defaultStates;
